9dc6ca43556119ea4eddbf41939353022e555641
Tanyapond <tanyaponddd@gmail.com>
2021-03-29 11:49:34 +0000

    Add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,16 @@ $(document).ready(function(){
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
   });
+
+$('.chips-autocomplete').chips({
+    autocompleteOptions: {
+      data: {
+        'Tools': null,
+        'Housewares': null,
+        'Wall-mounted': null,
+        'Equipment': null
+      },
+      limit: Infinity,
+      minLength: 1
+    }
+  });
